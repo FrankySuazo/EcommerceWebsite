fe4013f54ca6bf3af35f0e41fa2a9393410d988e
FrankySuazo <frankysuazo@outlook.com>
2024-04-10 22:41:36 -0400

    working progress

---
+++ pages/Home.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+//import product context
+import { ProductContext } from "../context/ProductContext";
+import Product from "../components/Product";
+
+const Home = () => {
+  //get product from product context
+  const { products } = useContext(ProductContext);
+
+  //get only men's & women's clothing category
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  return (
+    <div>
+      <section className="py-16">
+        <div className="container mx-auto">
+          {/* the grid size of the window changing sizes */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
+            {filteredProducts.map((product) => {
+              return <Product product={productkey={product.id}/>;
+              // <div
+              //color box background for product <<<<<<<<<<<<<<<
+              //       className="w-full h-[300px] bg-amber-200 mb-4"
+              //       key={product.id}
+              //     >
+              //       {product.title}
+              //     </div>
+              //   );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
